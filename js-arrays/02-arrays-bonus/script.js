2b3b1cf289912667f87217d5ff2fbdb76d02e246
ritaurbani <ritaurbani@gmail.com>
2024-11-03 22:47:06 +0100

    Bonus

--- js-arrays/02-arrays-bonus/script.js
+++ js-arrays/02-arrays-bonus/script.js
@@ -1,129 +1,75 @@
-// const teachers = [
-//   'Nathan',
-//   'Ed',
-//   'Fabio',
-//   'Phil',
-//   'Carlo',
-//   'Lewis',
-//   'Luca'
-// ]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
 
 // // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // // // e salva il risultato nella variabile reversedTeachers
 
-//  const reversedTeachers = [];
+ const reversedTeachers = [];
 
-// for ( let i = teachers.length -1; i>= 0; i--) {
-// const currentItem = teachers[i];
-// reversedTeachers.push(currentItem);
-// console.log(reversedTeachers);  /*SOLUZIONE INTERMEDIA*/
- 
+for ( let i = teachers.length -1; i>= 0; i--) {
+const currentItem = teachers[i];
+reversedTeachers.push(currentItem);
+console.log(reversedTeachers);  
 
+// 1 iterazione
+// i = 6
+// curItem = luca
+// i-- = 5
 
-/*
-1 iterazione
-i = 6
-curItem = luca
-i-- = 5
+// 2.iterazione
 
-2.iterazione
+// i = 5
+// currentitem =lewis
+// 4
 
-i = 5
-currentitem =lewis
-4
+// 3.iterazione
+// i=4
+// curritem = carlo
+// 3
 
-3.iterazione
-i=4
-curritem = carlo
-3
+// 4.iterazione
+// i=3
+// currentitem= phil
+// 2
 
-4.iterazione
-i=3
-currentitem= phil
-2
+// 5.iterazione
+// i = 2
+// currentitem = fabio
+// 1
 
-5.iterazione
-i = 2
-currentitem = fabio
-1
+// 6.iterazione
+// i = 1
+// currentitem = ed
+// 0
 
-6.iterazione
-i = 1
-currentitem = ed
-0
-
-7 iterazione
-i=0
-currentitem = Nathan
-
-
-
+// 7 iterazione
+// i=0
+// currentitem = Nathan
 
 
 // // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // // con un nome di lunghezza maggiore o uguale a 5 caratteri
-// const longNames = [];
 
-// for ( let i=0; i<teachers.length; i++) {
-//   console.log(`inizio iterazione: ${i}`)
-//   console.log(`teachers: ${teachers}`)
-//   console.log(`teachers[i]: ${teachers[i]}`)
-//   const currentItem = teachers[i];
-//   console.log(`currentItem: ${currentItem}`)
-//   if(currentItem.length >= 5) {
-//     console.log(`push Item: ${currentItem}`)  
-//     longNames.push(currentItem)
-//   } 
-//   console.log(`fine iterazione: ${i}`)
-// }
-// console.log(`longNames: ${longNames}`);
-// // 3. Rimuovi 'Ed' dall'array teachers
+const longNames = [];
+
+for ( let i=0; i<teachers.length; i++) {
+  const currentItem = teachers[i];
+  if(currentItem.length >= 5) {  
+    longNames.push(currentItem)
+  } 
+}
+console.log(`longNames: ${longNames}`);
 
 
-
-/*Calcolare la somma di numeri da 3 a 6;*/
-
-//  let sum = 0;   /* VARIABILE ACCUMULATORE*/
-
-// for (let i=3; i<=6; i++) {
-//   const currentNumber = i
-//  sum = sum+i
-//  console.log(sum)
-// }
-
-// --prima iterazione--
-// i = 3
-// sum = 0+i = 3
-// 
-// i++ = 1+1 = 4
-
-// --seconda iterazione
-
-// i = 4
-// sum = 3+i= 7
-// // i++= 5
-
-// //--terza iterazione
-
-// i= 5
-// sum = 3 +3 =6
-// i++ = 4
-
-// //--quarta iterazione
-
-// i = 4
-// sum = 6 + 3 = 9
-// i++ = 5
-
-// i = 5
-// sum = 9 + 5 = 14
-// i++ = 6
-
-// 1= 6
-// sum = 14 + 6 =20
-// i++ = 7
-
-********************************************************************
+//3. Rimuovi 'Ed' dall'array teachers
 
 
-
+const newTeacher = teachers.splice(1,1)
+console.log(teachers)
